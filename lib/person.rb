06a6babd9fefaa79e7caeb6d2b3ec984b98db695
yeah-right <claar.zack@gmail.com>
2019-03-13 15:19:32 -0600

    Halfway with @kevin892, swapping drivers

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,58 @@
 # your code goes here
+require 'pry'
+
+class Person
+  attr_accessor :bank_account, :happiness, :hygiene
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def happiness
+    if @happiness > 10
+      @happiness = 10
+    elsif @happiness < 0
+      @happiness = 0
+    end
+    @happiness
+  end
+
+  def hygiene
+    if @hygiene > 10
+      @hygiene = 10
+    elsif @hygiene < 0
+      @hygiene = 0
+    end
+    @hygiene
+  end
+
+  # PART 2 NON-ATTRIBUTE INSTANCE METHODS
+  def happy?
+    return @happiness > 7 ? true : false
+  end
+
+  def clean?
+    return @hygiene > 7 ? true : false
+  end
+
+  def get_paid(salary_amt)
+    @bank_account += salary_amt
+    return 'all about the benjamins'
+  end
+
+  # def hygiene=(hygiene)
+  #   @hygiene += 4
+  # end
+
+  def take_bath
+    # hygiene()
+    self.hygiene += 4
+    return "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+
+end
